9c09e4eb433ad91c35d1b3dcc36a9d493f3951c6
Becoming-a-CPR-Hero <cprrajarani@gmail.com>
2026-09-16 06:29:40 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -57,6 +57,7 @@ const KA_ASSETS_AVAILABLE = new Set([
   "ElevenLabs_2025-11-04T17_32_18_Alice_pre_sp100_s50_sb75_v3.mp3",
   "ElevenLabs_2025-06-28T05_17_33_Alice_pre_sp100_s50_sb75_v3.mp3",
   "ElevenLabs_2025-06-25T03_15_33_Alice_pre_sp100_s50_sb75_v3.mp3",
+  "ElevenLabs_2025-06-16T00_04_57_Alice_pre_sp100_s50_sb75_v3.mp3",
 ]);
 
 // Turns "filename.ext" into "filename_<lang>.ext" (unchanged for English,
